08ffc5821b65c629c6a61d1f53e9f2e5a0bcbb70
lemans77 <oleg.boiechko@gmail.com>
2025-02-17 13:27:05 +0200

    Repair files

---
+++ Scripts/setMaterialOrder.js
@@ -0,0 +1,125 @@
+﻿/**
+ * Скрипт на сортування елементів всередині моделі
+ */
+
+UnSelectAll();
+
+const forEachInList = (list) => {
+
+	// Рекурсивний обхід об'єкту
+	for (let i = list.Count - 1; i >= 0; i--) {
+		const elem = list[i];
+		if ((elem instanceof TFurnBlock || elem instanceof TDraftBlock) && !elem.JointData) {
+			forEachInList(elem);
+		};
+	};
+
+	// Лічильник
+	let counter = 0;
+
+	// Об'єкт для структурованого сортування
+	let obj = {
+		'arrSystem' : [],
+		'arrTFurnPanel' : [],
+		'arrTExtrusionBody' : [],
+		'arrT2DTrajectoryBody' : [],
+		'arrTFurnBlock' : [],
+		'arrFastSchemes' : [],
+		'arrTFurnAsm' : [],
+		'arrTFastener' : [],
+		'arrOther' : []
+	};
+
+	// Сортування посилань на об'єкти по типам
+	for (let i = 0; i < list.Count; i++) {
+		const item = list[i];
+		if (item instanceof TModelLimits) { obj.arrSystem.push(item) }
+		else if (item instanceof TFurnPanel) { obj.arrTFurnPanel.push(item) }
+		else if (item instanceof TExtrusionBody) { obj.arrTExtrusionBody.push(item) }
+		else if (item instanceof T2DTrajectoryBody) { obj.arrT2DTrajectoryBody.push(item) }
+		else if (item instanceof TFurnBlock && !item.JointData) { obj.arrTFurnBlock.push(item) }
+		else if (item instanceof TDraftBlock) { obj.arrTFurnBlock.push(item) }
+		else if (item instanceof TFurnBlock && item.JointData) { obj.arrFastSchemes.push(item) }
+		else if (item instanceof TFurnAsm) { obj.arrTFurnAsm.push(item) }
+		else if (item instanceof TFastener) { obj.arrTFastener.push(item) }
+		else { obj.arrOther.push(item); };
+	};
+
+	for (let key in obj) {
+
+		if (obj[key].lenght > 0) {
+			if (
+				key == 'arrTFurnPanel' ||
+				key == 'arrTExtrusionBody'
+			) {
+				// Приміняємо функцію сортування об'єктів по матеріалу
+				obj[key].sort(sortByField('MaterialName'));
+			};
+			
+			if (
+				key == 'arrFastSchemes' ||
+				key == 'arrTFurnAsm' ||
+				key == 'arrTFastener'
+			) {
+				// Приміняємо функцію сортування об'єктів по імені
+				obj[key].sort(sortByField('Name'));
+			};
+		};
+
+	};
+
+	// Змінюємо індекси елементів в структурі блока
+	for (const key in obj) counter = changeOwnerIndex(obj[key], counter);
+
+	// Записуємо зміни в історію
+	Undo.Changing(list);
+	list.Build();
+
+};
+
+/**
+ * Функція зміни позиції елемента в структурі блока (моделі)
+ * @param {List} arr
+ * @param {Number} counter
+ * @returns {Number}
+ */
+
+const changeOwnerIndex = (arr, counter) => {
+	for (let i = 0; i < arr.length; i++) {
+		const item = arr[i];
+		item.OwnerIndex = counter;
+		counter++;
+		Undo.Changing(item);
+		item.Build();
+	};
+	return counter;
+};
+
+/**
+ * Функція сортування по полю
+ * @param {String} Field
+ * @returns
+ */
+const sortByField = (field) => {
+	return (a, b) => a[field] > b[field] ? 1 : -1;
+};
+
+const main = () => {
+	Action.BlinkHint = 'Йде обробка';
+
+	if (Model.SelectionCount === 0) {
+		forEachInList(Model);
+	} else {
+		for (let i = 0; i < Model.SelectionCount; i++) {
+			const elem = Model.Selections[i];
+			if ((elem instanceof TFurnBlock || elem instanceof TDraftBlock) && !elem.JointData) {
+				forEachInList(elem.asList());
+			};
+		};
+	};
+	alert('Скрипт успішно здійснив обробку');
+};
+
+main();
+
+Action.Finish();
---
+++ Scripts/weightPanel.js
@@ -0,0 +1,23 @@
+﻿/**
+ * Вага панелей
+ */
+
+var weight = 0;
+
+Model.forEachPanel(function(panel) {
+  if ((panel.AsPanel) && (panel.Selected)) {
+    V = (panel.GSize.x) * (panel.GSize.y) * (panel.GSize.z) / 1000 / 1000;
+
+    den = 0;
+    if (panel.MaterialName.indexOf('ДСП') >= 0) {
+      den = 0.8;
+    } else if (panel.MaterialName.indexOf('ДВП') >= 0) {
+      den = 0.35;
+    } else if (panel.MaterialName.indexOf('МДФ') >= 0) {
+      den = 0.9;
+    }
+    weight = weight + V * den;
+  }
+})
+
+alert('Примерный вес панелей: ' + weight.toFixed(3) + ' кг');
